0261c5af77fcefa01176b1bb7422d05eb726580c
Robi <robi.maliqi@hotmail.co.uk>
2022-09-09 14:22:40 +0100

    second test passes

--- schoolReport.js
+++ schoolReport.js
@@ -1,5 +1,9 @@
 const schoolReport = (input) => {
-  return "Green: 1";
+  if (input === "Green") {
+    return "Green: 1";
+  } else {
+    return "Amber: 1";
+  }
 };
 
 module.exports = schoolReport;
--- schoolReport.test.js
+++ schoolReport.test.js
@@ -4,4 +4,8 @@ describe("schoolReport", () => {
   it("it returns an the amount of one input", () => {
     expect(schoolReport("Green")).toEqual("Green: 1");
   });
+
+  it("it returns an the amount of one input", () => {
+    expect(schoolReport("Amber")).toEqual("Amber: 1");
+  });
 });
