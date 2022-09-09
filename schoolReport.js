436141b97ec1dd81aed3c8f72164d36c957c2049
Robi <robi.maliqi@hotmail.co.uk>
2022-09-09 14:23:47 +0100

    third test passes and code refactored

--- schoolReport.js
+++ schoolReport.js
@@ -1,9 +1,5 @@
 const schoolReport = (input) => {
-  if (input === "Green") {
-    return "Green: 1";
-  } else {
-    return "Amber: 1";
-  }
+  return `${input}: 1`;
 };
 
 module.exports = schoolReport;
--- schoolReport.test.js
+++ schoolReport.test.js
@@ -8,4 +8,8 @@ describe("schoolReport", () => {
   it("it returns an the amount of one input", () => {
     expect(schoolReport("Amber")).toEqual("Amber: 1");
   });
+
+  it("it returns an the amount of one input", () => {
+    expect(schoolReport("Red")).toEqual("Red: 1");
+  });
 });
